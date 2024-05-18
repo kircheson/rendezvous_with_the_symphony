7c027d2c86ad7c83fe1567842ac50dfe6a378711
kircheson <kirill.kotlyaren@mail.ru>
2024-05-18 19:56:46 +0500

    Правка методов, шаблонов

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,8 +4,8 @@ namespace App\Controller;
 
 use App\Entity\TaskManagerEntity;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -35,16 +35,35 @@ class TaskController extends AbstractController
     }
 
     #[Route('/tasks', name: 'task_list')]
-    public function list()
+    public function list(EntityManagerInterface $em): Response
     {
-        $tasks = [];
+        $tasks = $em->getRepository(TaskManagerEntity::class)->findAll();
         return $this->render('task/list.html.twig', ['tasks' => $tasks]);
     }
 
     #[Route('/tasks/edit/{id}', name: 'task_edit', methods: ['GET', 'POST'])]
-    public function edit(?int $id)
+    public function edit(EntityManagerInterface $em, Request $request, ?int $id): Response
     {
-        return $this->render('task/edit.html.twig', ['task' => NULL]);
+        $task = $em->getRepository(TaskManagerEntity::class)->find($id);
+
+        if (!$task) {
+            throw $this->createNotFoundException('Задача по ID не найдена');
+        }
+
+        if ($request->isMethod('POST')) {
+            $title = $request->request->get('title');
+            $description = $request->request->get('description');
+            $email = $request->request->get('email');
+            $task->setTitle($title)
+                ->setDescription($description)
+                ->setEmail($email);
+
+            $em->flush();
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/edit.html.twig', ['task' => $task]);
     }
 
 }
